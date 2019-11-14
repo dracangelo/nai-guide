da19228d22dba808e4294bbc9534017b3409ec89
skimanikings <kimanisimon856@gmail.com>
2019-11-14 16:43:31 +0300

    add a button for finding guide

--- js/main.js
+++ js/main.js
@@ -1,55 +1,79 @@
 function getVariables() {
-    getstart = document.getElementById("start");
-    start = getstart.options[getstart.selectedIndex].value;
-    getdestination = document.getElementById("destination");
-    destination = getdestination.options[getdestination.selectedIndex].value;
-  }
-  function defineRoutes() {
-    routesArray = ["gpo", "cbd", "cic", "prestige", "adams",];
-  }
+  getstart = document.getElementById("start");
+  start = getstart.options[getstart.selectedIndex].value;
+  getdestination = document.getElementById("destination");
+  destination = getdestination.options[getdestination.selectedIndex].value;
+}
+function defineRoutes() {
+  routesArray = ["gpo", "cbd", "cic", "prestige", "adams"];
+}
 
-  $(document).ready(function() {
+function getDirections() {
+  locations = (start + destination);
+};
+  $("#button").click(function() {
     getVariables();
     defineRoutes();
-    $("#button").click(function() {
-        descriptionObject = {
-            aa : [],
-            ab : [],
-            ac : [],
-            ad : [],
-            ae : [], 
-            bb : [],
-            ba : [],
-            bc : [],
-            bd : [],
-            be : [],
-            ca : [],
-            cb : [],
-            cc : [],
-            cd : [],
-            ce : [],
-            da : [],
-            db : [],
-            dc : [],
-            dd:  [],
-            de : [],
-            ea : [],
-            eb : [],
-            ec : [],
-            ed : [],
-            ee : [],
-
-
-        }
-        locations = (start+destination);
-        guide=descriptionObject[locations][3];
-        guidesObject = {
-        aa : ["simon - 0713813919  ann - 0720384088"]
-        }
-        alert(descriptionObject[locations][0]);
-        alert(descriptionObject[locations][1]);
-        alert(descriptionObject[locations][2]);
-        alert(guidesObject[locations]);
-
-    });
+      getDirections();
+    descriptionObject = {
+      aa: [10],
+      ab: [20],
+      ac: [30],
+      ad: [],
+      ae: [],
+      bb: [],
+      ba: [],
+      bc: [],
+      bd: [],
+      be: [],
+      ca: [],
+      cb: [],
+      cc: [],
+      cd: [],
+      ce: [],
+      da: [],
+      db: [],
+      dc: [],
+      dd: [],
+      de: [],
+      ea: [],
+      eb: [],
+      ec: [],
+      ed: [],
+      ee: []
+    };
+    alert(descriptionObject[locations]);
+    
   });
+  $("#guide").click(function() {
+    getDirections();
+    guidesObject = {
+      aa: ["Simon - 0713813919  Ann - 0720384088"],
+      ab: ["Angelo - 070210098  Paddington-0740100369"],
+      ac: ["hamm-69 pam-70"],
+      ad: [],
+      ae: [],
+      bb: [],
+      ba: [],
+      bc: [],
+      bd: [],
+      be: [],
+      ca: [],
+      cb: [],
+      cc: [],
+      cd: [],
+      ce: [],
+      da: [],
+      db: [],
+      dc: [],
+      dd: [],
+      de: [],
+      ea: [],
+      eb: [],
+      ec: [],
+      ed: [],
+      ee: []
+    };
+    alert(guidesObject[locations]);
+  });
+
