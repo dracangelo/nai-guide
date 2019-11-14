83cabee2f92050b2a772fd78fefaeb983b9c30e5
Mark Watitwa <markwatitwa98@gmail.com>
2019-11-14 23:05:43 +0300

    Added typed js to landing page

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var typed = new Typed('.type', {
+    strings: [
+        'Faster',
+        'Easier',
+        'In a more convenient way',
+        'Haslle free'
+    ],
+    typeSpeed:60,
+    backSpeed:60,
+    loop: true 
+  });
