801069f459366e5b14739bbfd8f3264f622f09c0
Mark Watitwa <markwatitwa98@gmail.com>
2019-11-15 06:45:11 +0300

    Linking

--- js/script.js
+++ js/script.js
@@ -1,11 +1,16 @@
 var typed = new Typed('.type', {
     strings: [
-        'Faster',
-        'Easier',
-        'In a more convenient way',
-        'Haslle free'
+        'Faster.',
+        'Easier.',
+        'In a more convenient way.',
+        'Haslle free.'
     ],
     typeSpeed:60,
     backSpeed:60,
     loop: true 
+  });
+
+  $(document).on("click", '[data-toggle="lightbox"]', function(event) {
+    event.preventDefault();
+    $(this).ekkoLightbox();
   });
